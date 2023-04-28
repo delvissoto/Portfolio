8c3d8f748e7ecb670d9916965e55c7f52bf03363
delvis Soto <delvissoto99@icloud.com>
2023-04-27 20:33:45 -0500

    footer fixes and marging

--- pages/Proyects.js
+++ pages/Proyects.js
@@ -12,10 +12,10 @@ const Proyects = () => {
         <Image className='portfolioImage' src={myWebSiteOne}/>
         
         <div className='portfoliotext'>
-          <h3>lore ipsumn</h3>
+          <h3>ChitChat</h3>
           <p  className='lightgreytext'>lorem ipsumn Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis sint eum vero numquam cum enim sequi id mollitia et, inventore nisi quos natus officiis dolorem facere optio repudiandae atque laudantium.</p>
-          <a className='lenguageused'>React</a> <a className='lenguageused'>SCSS</a> <br/>
-        <a className='codeDemo'>Code </a> <a className='codeDemo'>Live Demo</a>
+          <a className='lenguageused'>Next JS</a> <a className='lenguageused'>Postgresql</a> <br/>
+        <a href='https://github.com/delvissoto/chitchat' className='codeDemo'>Code </a> <a className='codeDemo'>Live Demo</a>
         </div>
         
      </div>
@@ -25,10 +25,10 @@ const Proyects = () => {
         
         
         <div className='portfoliotext'>
-          <h3>lore ipsumn</h3>
+          <h3>Gaming Social</h3>
           <p  className='lightgreytext'>lorem ipsumn Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis sint eum vero numquam cum enim sequi id mollitia et, inventore nisi quos natus officiis dolorem facere optio repudiandae atque laudantium.</p>
-          <a className='lenguageused'>React</a> <a className='lenguageused'>SCSS</a> <br/>
-        <a className='codeDemo'>Code </a> <a className='codeDemo'>Live Demo</a>
+          <a className='lenguageused'>React</a> <a className='lenguageused'>CSS</a> <br/>
+        <a href='https://github.com/delvissoto/gaming_social' className='codeDemo'>Code </a> <a className='codeDemo'>Live Demo</a>
         </div>
         <Image className='portfolioImage' src={myWebSiteOne}/>
         
