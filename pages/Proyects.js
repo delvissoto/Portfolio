2e85413ad826b7792d2fa6d19eadf849af4918db
delvissoto <delvissoto99@icloud.com>
2024-01-17 19:44:07 -0600

    added one game.

--- pages/Proyects.js
+++ pages/Proyects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import myWebSiteOne from '../public/websiteDesktop.png'
+import shibgame from '../public/shibgame.png'
 
 
 const Proyects = () => {
@@ -71,12 +72,12 @@ const Proyects = () => {
        
         
         <div className='portfoliotext'>
-          <h3>lore ipsumn</h3>
-          <p  className='lightgreytext'>lorem ipsumn Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis sint eum vero numquam cum enim sequi id mollitia et, inventore nisi quos natus officiis dolorem facere optio repudiandae atque laudantium.</p>
-          <a className='lenguageused'>React</a> <a className='lenguageused'>SCSS</a> <br/>
-        <a className='codeDemo'>Code </a> <a className='codeDemo'>Live Demo</a>
+          <h3>Whack a Shib</h3>
+          <p  className='lightgreytext'>This is just a game that is being made with vanillaJs, CSS and HTML here you can hit Shib dogs in a rocker and try to get the do better every time you played it. </p>
+          <a className='lenguageused'>JavaScript</a> <a className='lenguageused'>CSS</a><a className='lenguageused'>HTML</a> <br/>
+        <a href='https://github.com/delvissoto/Whack-a-Shib' className='codeDemo'>Code </a> <a href='https://whack-a-shib.vercel.app/' className='codeDemo'>Live Demo</a>
         </div>
-        <Image className='portfolioImage' src={myWebSiteOne}/>
+        <Image className='portfolioImage' src={shibgame}/>
         
      </div>
     </div>
